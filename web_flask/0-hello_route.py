f781d4674951f23470ae0258538f5a41199ba9a4
Awoyemi Boluwatife <81172907+Bowlu@users.noreply.github.com>
2023-01-13 20:12:00 +0100

    fixed error

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -2,14 +2,18 @@
 
 """ Starting a Flask web application that
 listens on 0.0.0.0 port 5000"""
-
 from flask import Flask
+
+
 app = Flask(__name__)
+
 
 @app.route('/', strict_slashes=False)
 def hello():
     """ Function that returns Hello HBNB"""
+    
     return "Hello HBNB!"
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0" , port=5000) 
