b034297ea4e29a98016f5d8737212f46d3c6b438
Awoyemi Boluwatife <81172907+Bowlu@users.noreply.github.com>
2023-01-13 20:51:34 +0100

    error perfectly fixed

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,20 +1,23 @@
 #!/usr/bin/python3
-
-""" Starting a Flask web application that
-listens on 0.0.0.0 port 5000"""
-
+"""Flask web application"""
 from flask import Flask
+
+
 app = Flask(__name__)
+
 
 @app.route('/', strict_slashes=False)
 def hello():
+    """Displaying Hello HBNB!"""
+
     return "Hello HBNB!"
 
-
-"""another route added"""
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
+    """Displaying HBNB!"""
+
     return "HBNB"
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0" , port=5000) 
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000)
