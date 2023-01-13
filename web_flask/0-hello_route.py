99027aa329e8c4373ea15ee3fe336cde19688a7b
Awoyemi Boluwatife <awoyemiboluwatife@gmail.com>
2023-01-13 19:44:06 +0000

    trying to fix error

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" Starting a Flask web application"""
+"""Flask web application"""
 from flask import Flask
 
 
@@ -8,10 +8,10 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hello():
-    """ Function that returns Hello HBNB"""
-    
+    """Displaying Hello HBNB!"""
+
     return "Hello HBNB!"
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000) 
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000, debug=True)
