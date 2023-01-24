0f2daf5f3cc70db65cad7d33544761f333a446eb
Awoyemi Boluwatife <81172907+Bowlu@users.noreply.github.com>
2023-01-24 22:19:02 +0100

    adjusted error

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -4,6 +4,7 @@ import os.path
 from datetime import datetime
 from fabric.api import local
 
+
 def do_pack():
     """Create a tar gzipped archive of the directory web_static."""
     dt = datetime.utcnow()
@@ -13,6 +14,8 @@ def do_pack():
                                                          dt.hour,
                                                          dt.minute,
                                                          dt.second)
+    
+    
     if os.path.isdir("versions") is False:
         if local("mkdir -p versions").failed is True:
             return None
